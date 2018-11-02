d74ef9f2c92d69a61d8424c1725a100797dec868
Kacper Mazur <33032053+kacpermazur@users.noreply.github.com>
2018-11-02 10:28:18 +0000

    push
    Bug: Colliders

--- Unity Door Project/Door/Assets/Scripts/DoorScript.cs
+++ Unity Door Project/Door/Assets/Scripts/DoorScript.cs
@@ -1,36 +1,130 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.XR.Interaction;
+using UnityEngine.UI;
 
 public class DoorScript : MonoBehaviour
 {
 
-	public List<GameObject> doorCubes;
-	public GameObject centerCube;
-	private Vector3 m_center;
-	public float sinSpeed;
-	public float sinDistance;
+	[SerializeField] private List<GameObject> _doorCubes;
 
+	[SerializeField] private List<Vector3> _cubeOriginalPosition;
+	[SerializeField] private List<Vector3> _pathWayPoints;
 
-    void Awake()
-    {
-        m_center = centerCube.transform.position;
-    }
-    void Start()
+	[SerializeField] private GameObject _centerCube;
+	private Vector3 _centerCubePos;
+
+	[SerializeField] private Text _text;
+
+[Header("Door Values")]
+	[SerializeField] private float _sinSpeed;
+	[SerializeField] private float _sinDistance;
+	[SerializeField] private float _blockMoveSpeed = 0.1f;
+
+	public bool playerTirggerClose = false;
+	
+
+	enum States
+	{
+		Open,
+		Opened,
+		Close
+	}
+	
+	States DoorState;
+	
+
+	void Awake()
 	{
 		
+		
+		for (int i = 0; i < _doorCubes.Count; i++)
+		{
+			_cubeOriginalPosition.Add(_doorCubes[i].transform.position);
+		}
+
+		DoorState = States.Close;
 	}
 
-	void Update()
+	void Start()
+    {
+	    foreach (GameObject e in GameObject.FindGameObjectsWithTag("PathwayPoint"))
+	    {
+		    _pathWayPoints.Add(e.transform.position);
+	    }
+	    
+	    
+	    if (_centerCube != null)
+	    {
+		    _centerCubePos = _centerCube.transform.position;
+	    }
+    }
+
+
+	void FixedUpdate()
 	{
-		for (int i = 0; i < doorCubes.Count; i++)
+
+		for (int i = 0; i < _doorCubes.Count; i++)
 		{
-			float dist = Vector3.Distance(m_center, new Vector3(doorCubes[i].transform.position.x, doorCubes[i].transform.position.y, 0.0f));
-			float sinVal = Mathf.Sin(dist * sinSpeed * Time.time);
-			float finalVal = sinVal * sinDistance;
-			Vector3 someVal = doorCubes[i].transform.transform.position;
-			someVal.z = someVal.z + finalVal;
-			doorCubes[i].transform.position = someVal;
+			if (DoorState == States.Close)
+			{
+				Idle(i);
+			}
+			else if (DoorState == States.Open)
+			{
+				Open(i);
+				DoorState = States.Opened;
+			}
+			else if (DoorState == States.Opened && playerTirggerClose)
+			{
+				Close(i);
+				DoorState = States.Close;
+			}
+			else
+			{
+				Debug.Log("something broke");
+			}
+		}
+	}
+
+
+	private void Idle(int currentCube)
+	{
+		float cubeDistance = Vector3.Distance(_centerCubePos,
+			new Vector3(_doorCubes[currentCube].transform.position.x, _doorCubes[currentCube].transform.position.y, 0F));
+
+		float sinValue = Mathf.Sin(cubeDistance * _sinSpeed * Time.time);
+		float sinDistance = sinValue * _sinDistance;
+
+		Vector3 calcuatedValue = _doorCubes[currentCube].transform.position;
+		calcuatedValue.z += sinDistance;
+
+		_doorCubes[currentCube].transform.position = calcuatedValue;
+	}
+
+	private void Open(int e)
+	{
+		_doorCubes[e].transform.position = Vector3.MoveTowards(_doorCubes[e].transform.position, _pathWayPoints[e], _blockMoveSpeed);
+	}
+
+	private void Close(int e)
+	{
+		_doorCubes[e].transform.position = Vector3.MoveTowards(_doorCubes[e].transform.position, _cubeOriginalPosition[e], _blockMoveSpeed);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			_text.enabled = true;
+
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				DoorState = States.Open;
+			}
+
 		}
 	}
 }
---
+++ Unity Door Project/Door/Assets/Scripts/GeneratePathWay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePathWay : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _instanceOFCord;
+
+	[SerializeField] private float _amountOFBlocksSide;
+	[SerializeField] private float _amountOFBlocksFoward;
+	[SerializeField] private float _offset = 0.35f;
+	
+
+
+	void Awake()
+	{
+		for (int i = 0; i < _amountOFBlocksSide; i++)
+		{
+			for (int j = 0; j < _amountOFBlocksFoward; j++)
+			{
+				Instantiate(_instanceOFCord, new Vector3(-4 + i * _offset , 0 , j * -_offset - 5.35f), Quaternion.identity);
+			}
+		}
+
+	}
+	
+
+}
